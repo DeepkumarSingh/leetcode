45d8605ab3775e4bc582485daa6bd472a02edbfc
Deep kumar <141671549+DeepkumarSingh@users.noreply.github.com>
2024-03-02 14:31:06 +0530

    Update Peak Index in a Mountain Array.cpp

--- Sorting & Searching /Peak Index in a Mountain Array.cpp
+++ Sorting & Searching /Peak Index in a Mountain Array.cpp
@@ -5,7 +5,7 @@ public:
         int start=0;
         int end=size-1;
         //int mid = start+(end-start)/2;
-        while(start<end){ // Special Case where start is less than end (start<end)
+        while(start<end){ // Special Case where start is less than end (start<end)-- To avoid infinite Loop 
             int mid = start+(end-start)/2;
             if(arr[mid]<arr[mid+1]){ // means we are in part A(increasing function)
             // if we are in part A and we have to find peak index just go to Right part
